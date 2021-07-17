8f025e27ad923d9adcc6bfae4e441045b145411d
kensuke suzuki <35k.0101@gmail.com>
2021-07-17 22:21:32 +0900

    install pug / nodemon

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
+// Home route
 app.get("/", (req, res) => {
-  res.send("Hello World")
+  res.render("index", {
+    title: "Hello"
+  })
 })
 
 app.listen(3000, () => {
