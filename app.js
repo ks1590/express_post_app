5d3d60e552740b2484056cce8e294a9dae28f5a4
kensuke suzuki <35k.0101@gmail.com>
2021-07-18 21:26:34 +0900

    add:mongoDB

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require("express")
 const path = require("path")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/express_post_app")
+let db = mongoose.connection
+
+db.once("open", () => {
+  console.log("Connected to MongoDB")
+})
+
+db.on("error", (err) => {
+  console.log(err)
+})
+
 const app = express()
+
+let Article = require("./models/article")
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 
 // Home route
 app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Hello"
+  Article.find({}, (err, articles) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render("index", {
+        title: "Articles",
+        articles: articles
+      })
+    }
+  })
+})
+
+app.get("/articles/add", (req, res) => {
+  res.render("add_article", {
+    title: "Add Article"
   })
 })
 
